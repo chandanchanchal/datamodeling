11ba9b193a631ed56b7e5b0224f57d412ac46344
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-03-17 14:56:21 +0530

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -263,6 +263,18 @@ Step 1: Conceptual Model (High-Level Design)
 Entities & Relationships
 In the travel domain, key entities and their relationships can be defined as follows:
 
+Traveler: A person booking trips.
+Trip: Represents a journey taken by a traveler.
+Destination: Places where trips are made.
+Booking: A record of a trip booked by a traveler.
+Payment: Details of payments made for bookings.
+Transportation: Modes of transport (Flight, Train, Bus, etc.).
+Accommodation: Hotels or places travelers stay.
+Review: Ratings & feedback by travelers.
+
+
+Step 2: Logical Model (Detailed Attributes & Relationships)
+Now, we define attributes and relationships.
 
 
 
